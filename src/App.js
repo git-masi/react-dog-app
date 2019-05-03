308c474d9ee5fc5a12108bd03eb509bd1b7c4165
Eric Masi <eric@gitmasi.com>
2019-05-03 09:37:37 -0400

    set basic routes and default props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Dogs from './components/Dogs';
+import Dog from './components/Dog';
+import whiskey from './assets/images/whiskey.jpg'
+import hazel from './assets/images/hazel.jpg'
+import tubby from './assets/images/tubby.jpg'
 
-function App() {
+function App(props) {
+
   return (
-    <div></div>
+    <div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/dogs" render={() => <Dogs dogs={props.dogs}/>}/>
+        <Route exact path="/dogs/:name" render={routeProps => <Dog {...routeProps} />}/>
+        <Route render={() => <h1>Oops, page not found</h1>} />
+      </Switch>
+    </div>
   );
+}
+
+App.defaultProps = {
+  dogs: [
+    {
+      name: "Whiskey",
+      age: 5,
+      src: whiskey,
+      facts: [
+        "Whiskey loves eating popcorn.",
+        "Whiskey is a terrible guard dog.",
+        "Whiskey wants to cuddle with you!"
+      ]
+    },
+    {
+      name: "Hazel",
+      age: 3,
+      src: hazel,
+      facts: [
+        "Hazel has soooo much energy!",
+        "Hazel is highly intelligent.",
+        "Hazel loves people more than dogs."
+      ]
+    },
+    {
+      name: "Tubby",
+      age: 4,
+      src: tubby,
+      facts: [
+        "Tubby is not the brightest dog",
+        "Tubby does not like walks or exercise.",
+        "Tubby loves eating food."
+      ]
+    }
+  ]
 }
 
 export default App;
---
+++ src/components/Dogs.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+  
+class Dogs extends Component {
+  render() {
+    return (
+      <div>
+        <div class="text-center">
+          <img src="..." class="rounded-circle" alt="..." />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Dogs;
